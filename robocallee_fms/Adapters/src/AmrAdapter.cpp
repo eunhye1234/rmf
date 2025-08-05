8d210a0100e720c3a6f5f2368a396ec896d3dcdb
eunhye1234 <101087291+eunhye1234@users.noreply.github.com>
2025-08-05 16:58:07 +0900

    Add files via upload

--- robocallee_fms/Adapters/src/AmrAdapter.cpp
+++ robocallee_fms/Adapters/src/AmrAdapter.cpp
@@ -6,6 +6,8 @@ AmrAdapter::AmrAdapter(Integrated::w_ptr<core::ICore> Icore, Logger::s_ptr log, 
     :Icore_(Icore),log_(log)
 {
     log_->Log(Log::LogLevel::INFO,"AmrAdapter 객체 생성");
+
+    robot_task_info_.robot_id = name;
 }
 
 AmrAdapter::~AmrAdapter()
@@ -13,29 +15,52 @@ AmrAdapter::~AmrAdapter()
     log_->Log(Log::LogLevel::INFO,"AmrAdapter 객체 소멸");
 }
 
-void Adapter::UpdateTaskInfo(const Commondefine::RequestInfo& request)
+
+Commondefine::RobotTaskInfo& AmrAdapter::GetTaskInfo()
 {
-    // if (request.requester=="customer"){
-    //     // task_info_.dest1 = 창고
-    //     // task_info_.dest2 = 배달지
-        
-    // }
-    // else if (request.requester=="employee"){
-        // task_info_.dest1 = 수거함
-        // task_info_.dest2 = 창고
-        
-    // }
-
-    task_info_.id = request.id;
-    task_info_.shoes_property = request.shoes_proptery;
-   
-    task_info_.requester = request.requester;
-    task_info_.customer_id = request.customer_id;
-
-    
+    return robot_task_info_;
 }
 
-Commondefine::RobotTaskInfo& GetTaskInfo()
+void AmrAdapter::SetTaskInfo(const Commondefine::GUIRequest& request)
 {
-    return task_info_;
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+
+        // if (request.requester=="customer"){
+        //     robot_task_info_.dest1 = 창고
+        //     robot_task_info_.dest2 = 배달지
+            
+        // }
+        // else if (request.requester=="employee"){
+        //     robot_task_info_.dest1 = 수거함
+        //     robot_task_info_.dest2 = 창고
+            
+        // }
+
+        robot_task_info_.robot_state = Commondefine::RobotState::BUSY;
+        robot_task_info_.shoes_property = request.shoes_property;
+    
+        robot_task_info_.requester = request.requester;
+        robot_task_info_.customer_id = request.customer_id;
+
+        std::ostringstream oss;
+        oss << ",color=" << robot_task_info_.shoes_property.color
+            << ", model=" << robot_task_info_.shoes_property.model
+            << ", size=" << robot_task_info_.shoes_property.size
+            << ",pinky_id=" << robot_task_info_.robot_id;
+
+        log_->Log(Log::LogLevel::INFO, oss.str());
+        log_->Log(Log::LogLevel::INFO,"SetTaskInfo 완료");
+
+    }
+
+}
+
+void AmrAdapter::SetAmrState(const Commondefine::RobotState& state)
+{
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        
+        robot_task_info_.robot_state = state;
+    }
 }
